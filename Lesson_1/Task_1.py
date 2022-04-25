689b8604cc80fc79e8df397261ce7aa1a04b5c4d
George <georgiy.miklaush@gmail.com>
2022-04-25 20:06:15 +0300

    Task_1

--- Lesson_1/Task_1.py
+++ Lesson_1/Task_1.py
@@ -4,13 +4,7 @@
 """
 from math import prod
 
-
-def evaluate_sum_product(user_input):
-    number_list = [int(el) for el in user_input]
-    return print(f'Для введённого числа: {user_input}, сумма = {sum(number_list)}, произведение = {prod(number_list)}')
-
-
-while (user_input := input('Введите трёхзначное число: ')) != '':
+while (user_input := input('Введите трёхзначное число(для завершения введите пустое значение): ')) != '':
     if user_input.isdigit() and len(user_input) == 3:
-        evaluate_sum_product(user_input)
-        break
+        number_list = [int(el) for el in user_input]
+        print(f'Для введённого числа: {user_input}, сумма = {sum(number_list)}, произведение = {prod(number_list)}')
